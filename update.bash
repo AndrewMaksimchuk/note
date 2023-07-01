465caf66d2132326ba54cbee35db2ec413c9e792
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-07-01 11:44:17 +0300

    add/change project files

--- update.bash
+++ update.bash
@@ -1,23 +1,32 @@
 #!/usr/bin/bash
 
 
-notesfile=$(git status -s ./content)
-if [[ -n $notesfile ]]; then
-    echo "[ Add notes ]"
-    git add ./content/*
-    counter=$(echo $notesfile | wc -l)
-    message=$(echo "add/change $counter notes")
-    git commit -m "$message"
-fi
+cwd=$(dirname $0)
 
 
-projectfiles=$(git status -s)
-if [[ -n $projectfiles ]]; then
-    echo "[ Add project files ]"
-    git add .
-    git commit -m "add/change project files"
-fi
+function update() {
+    notesfile=$(git status -s ./content)
+    if [[ -n $notesfile ]]; then
+        echo "[ Add notes ]"
+        git add ./content/*
+        counter=$(echo $notesfile | wc -l)
+        message=$(echo "add/change $counter notes")
+        git commit -m "$message"
+    fi
 
 
-echo "[ Push to remote repository ]"
-git push origin main
+    projectfiles=$(git status -s)
+    if [[ -n $projectfiles ]]; then
+        echo "[ Add project files ]"
+        git add .
+        git commit -m "add/change project files"
+    fi
+
+
+    echo "[ Push to remote repository ]"
+    git push origin main
+}
+
+
+result=$(cd $cwd && update)
+echo $result
