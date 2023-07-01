12626e209e47d8c884824cb511e6151bc888bc2b
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-07-01 11:33:30 +0300

    add/change project files

--- select.bash
+++ select.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 
+# Without argument this script show menu 
+# to select tag
+# 
+# With argument(tag name) - show random note 
+# from tag
+
+
 cwd=$(dirname $0)
 tags=$(cat $cwd/tags)
 quite="exit"
@@ -10,6 +17,27 @@ PS3="Select tag: "
 . $cwd/colors.bash
 
 
+function shownote() {
+    dir=$(echo $cwd/content/$1)
+    isEmpty=$(ls -A $dir)
+    if [[ -z $isEmpty ]]; then
+        echo "You don\`t have note in selected tag"
+        exit
+    fi
+    files=($dir/*)
+    randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+    echo
+    echo -e "${yellow}Note: $randomfile ${reset}"
+    cat $randomfile | fold -w 80 -s
+    exit
+}
+
+
+if [ $# -eq 1 ]; then
+    shownote $1    
+fi
+
+
 select tag in $quite $tags
 do
     if [[ $tag = $quite ]]; then
@@ -17,17 +45,6 @@ do
     fi
 
     if [[ -n $tag ]]; then
-        dir=$(echo $cwd/content/$tag)
-        isEmpty=$(ls -A $dir)
-        if [[ -z $isEmpty ]]; then
-            echo "You don\`t have note in selected tag"
-            exit
-        fi
-        files=($dir/*)
-        randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-        echo
-        echo -e "${yellow}Note: $randomfile ${reset}"
-        cat $randomfile | fold -w 80 -s
-        exit
+        shownote $tag
     fi
 done
