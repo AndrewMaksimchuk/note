40a724d23db36c3a00783e2bca7367fc6bcbe232
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-07-26 12:16:25 +0300

    check if cron job already setup

--- autoupdate.bash
+++ autoupdate.bash
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-
 cwd=$(realpath $(dirname $0))
 
 cron_file="$cwd/cron"
 update_script_file="$cwd/update.bash"
 
-crontab -l > $cron_file
+crontab -l >$cron_file
 
-echo "00 10 * * 1-5 $update_script_file" >> $cron_file
+isSetup=$(cat $cron_file | grep $update_script_file | wc -l)
+if [[ $isSetup -ge 1 ]]; then
+    echo "Job already setup"
+    exit
+fi
+
+echo "00 10 * * 1-5 $update_script_file" >>$cron_file
 
 crontab $cron_file
