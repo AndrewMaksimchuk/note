0523747799817fc01e897d9a76096d36fefcba21
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-11-23 16:31:56 +0200

    add number for each finded lines
    and show file that find by put number to search command

--- editors.bash
+++ editors.bash
@@ -8,6 +8,6 @@ open_by_vscode='code --goto <file:line[:character]>'
 function use_editor
 {
     echo "For open file in specific line in editor"
-    echo -e "\033[1;4mvim:\033[0m $open_by_vi"
+    echo -e "\033[1;4mvi/vim:\033[0m $open_by_vi"
     echo -e "\033[1;4mvscode:\033[0m $open_by_vscode"
 }
--- search.bash
+++ search.bash
@@ -5,7 +5,10 @@
 # Arguments:
 #    $1 - word/pattern grep
 #    $2 - tag(option)
-
+# 
+# If $1 is "open" then open file from result 
+# table, saved in ".search_history" file
+# where $2 is number of file from COUNT column
 
 projectdir=$(dirname $0)
 
@@ -14,9 +17,36 @@ projectdir=$(dirname $0)
 . $projectdir/editors.bash
 
 SCREEN_WIDTH=`tput cols`
+HISTORY_FILE="$projectdir/.search_history"
+
+function open_file
+{
+    if [[ $1 = "open" ]]; then
+        if [[ -n $2 ]]; then
+            local file_path=$(tail -n+2 $HISTORY_FILE | grep -w -e "^ *$2" | cut -f2 -d'|' | sed 's/file:\/\///g')
+            vi $file_path
+            exit
+        fi
+    fi
+}
+
+function use_search
+{
+    echo "For open file from search use next command:"
+    echo "note search open number_of_file_COUNT_column"
+}
+
+function save
+{
+    cat $1 > "$HISTORY_FILE"
+}
 
 function output
 {
+    if [[ -z $1 ]]; then
+        echo "Nothing found"
+        exit
+    fi
     local files=$(echo "$1" | cut -f1 -d '|' | tail -n+1 | sed 's/file:\/\///g')
     local lines=""
     
@@ -29,18 +59,24 @@ function output
     local TABLE_LEFT_FILE=$(mktemp)
     local TABLE_RIGHT_FILE=$(mktemp)
     local TABLE_RESULT_FILE=$(mktemp)
-    echo "$1" > $TABLE_LEFT_FILE
+    local TABLE_COUNTER=$(mktemp)
+    echo "$1" | nl  -v1 -w3 -s". " | cut -f1 -d. > $TABLE_COUNTER
+    echo "$1"  | tr -s '[:blank:]' > $TABLE_LEFT_FILE
+    res=$(paste -d "|" $TABLE_COUNTER $TABLE_LEFT_FILE)
+    echo "$res" > $TABLE_LEFT_FILE
     echo -e "$first_lines" > $TABLE_RIGHT_FILE
 
     paste -d "" $TABLE_LEFT_FILE $TABLE_RIGHT_FILE \
     | column -t -s "|" --output-separator " | " \
-    -T 3 -c $SCREEN_WIDTH \
-    --table-columns FILE,LINE,CONTENT,HEADER \
-    --table-right LINE \
+    -T 4 -c $SCREEN_WIDTH \
+    --table-columns COUNT,FILE,LINE,CONTENT,HEADER \
+    --table-right LINE --table-right COUNT \
     > $TABLE_RESULT_FILE
 
     cat $TABLE_RESULT_FILE
-    [[ -n "$1" ]] && use_editor
+    save $TABLE_RESULT_FILE
+    use_editor
+    use_search
 }
 
 function format_left_table
@@ -80,12 +116,11 @@ function search_note
     fi
 
     local result=$(
-        local files_list=$(grep -I -e $1 -rwn $projectdir/content/**/*)
+        files_list=$(grep -I -e $1 -rwn $projectdir/content/**/*)
         format_left_table "$files_list"
     )
-
     output "$result"
 }
 
-
+open_file $1 $2
 search_note $1 $2
