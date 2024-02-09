2261b4baa4c5c6c0286c19cbef0b560c07822147
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-02-09 16:36:17 +0200

    add 'present' 'fullscreen' mode and toggle nav

---
+++ web_site/modules/nav.api.js
@@ -0,0 +1,13 @@
+export function toggleNavPanel() {
+  const buttonHide = document.getElementById("button-hide");
+  const root = document.querySelector(":root");
+  const rs = getComputedStyle(root);
+
+  if (rs.getPropertyValue("--nav-width") === "50ch") {
+    root.style.setProperty("--nav-width", "1ch");
+    buttonHide.firstElementChild.textContent = "Show panel";
+  } else {
+    root.style.setProperty("--nav-width", "50ch");
+    buttonHide.firstElementChild.textContent = "Hide panel";
+  }
+}
