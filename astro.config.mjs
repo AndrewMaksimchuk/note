e5a5a91f6cf21086cb9a9e4da67f70d368bbeb62
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-02-09 11:07:52 +0200

    restore output directory, fix highlight url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ export default defineConfig({
     base: process.env["NODE_ENV"] === "dev" ? undefined : '/note',
     srcDir: "./web_site",
     publicDir: "./web_public",
-    outDir: "./web_static",
     output: "static",
     build: {
         format: "file"
