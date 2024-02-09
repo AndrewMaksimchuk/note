0003cb0d0a0a6434272bb08640a6f9d5b1ca2888
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-02-09 09:59:13 +0200

    github pages + action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
+    site: 'https://AndrewMaksimchuk.github.io',
+    base: '/note',
     srcDir: "./web_site",
     publicDir: "./web_public",
     outDir: "./web_static",
     output: "static",
     build: {
-        format: "file" // https://docs.astro.build/en/reference/configuration-reference/#buildformat
+        format: "file"
     },
     markdown: {
         syntaxHighlight: "shiki",
@@ -16,6 +17,6 @@ export default defineConfig({
         },
     },
     devToolbar: {
-        enabled: true,
+        enabled: false,
     },
 });
