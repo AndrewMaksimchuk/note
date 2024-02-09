ee049ac2a8768b25300dfc63605b41d80ea8eaa6
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-02-09 10:25:49 +0200

    remove abs path to tags file, update base property

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://AndrewMaksimchuk.github.io',
-    base: '/note',
+    base: process.env["NODE_ENV"] === "dev" ? undefined : '/note',
     srcDir: "./web_site",
     publicDir: "./web_public",
     outDir: "./web_static",
