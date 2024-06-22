ae935bf5cc3d540f25f2a6bb0f62c5846339f2c6
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-06-22 23:21:23 +0300

    [web] - update styles for header text and button for hide navigation panel #57

--- web_site/modules/nav.api.js
+++ web_site/modules/nav.api.js
@@ -6,9 +6,11 @@ export function toggleNavPanel(callback) {
   if (rs.getPropertyValue("--nav-width") === "50ch") {
     root.style.setProperty("--nav-width", "1ch");
     buttonHide.firstElementChild.textContent = "Show panel";
+    buttonHide?.setAttribute("title", "Click for show side panel");
   } else {
     root.style.setProperty("--nav-width", "50ch");
     buttonHide.firstElementChild.textContent = "Hide panel";
+    buttonHide?.setAttribute("title", "Click for hide side panel");
   }
 
   callback();
